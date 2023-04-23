ee615d00b1f0bbfca81e483c9cf8c81ba3240e1e
Ventsislav <vncnklv0@gmail.com>
2023-04-23 21:14:38 +0300

    Styling header and login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <header className="h-16 shadow-md flex items-center justify-around mb-36">
-            <Link to={'/'} className="text-2xl text-primary font-bold">BodyBalance</Link>
+            <Link to={'/'} className="text-2xl text-primary font-bold">BodyBalance.</Link>
             <div className="flex gap-2">
-                <Link to={'/login'} className="font-medium px-6 py-2 text-gray-800 hover:text-black">Login</Link>
-                <Link to={'/register'} className="font-medium bg-secondary rounded-full px-10 py-2 text-gray-800 hover:text-black">Register</Link>
+                <Link to={'/login'} className="font-medium px-6 py-2 text-gray-800 hover:text-black text-sm">LOG IN</Link>
+                <Link to={'/register'} className="font-medium bg-secondary rounded-full px-10 py-2 text-gray-800 text-sm hover:text-black ">SIGN UP</Link>
             </div>
         </header>
     )
